0c6f799141713d11510be2950c447dc8a0fab12c
JeroMiya <bell.jeremy@gmail.com>
2016-03-10 18:12:15 -0500

    Cleaned up frontend project. Removed unnecessary minify/uglify/concatenation gulp tasks. Added a clean:generated task to remove generated files. Added angular dependency to bower.json.

--- src/ConfigUtil/gulpfile.js
+++ src/ConfigUtil/gulpfile.js
@@ -2,44 +2,17 @@
 "use strict";
 
 var gulp = require("gulp"),
-    rimraf = require("rimraf"),
-    concat = require("gulp-concat"),
-    cssmin = require("gulp-cssmin"),
-    uglify = require("gulp-uglify");
+    rimraf = require("rimraf");
 
 var paths = {
-    webroot: "./wwwroot/"
+	webroot: "./wwwroot/"
 };
 
-paths.js = paths.webroot + "js/**/*.js";
-paths.minJs = paths.webroot + "js/**/*.min.js";
-paths.css = paths.webroot + "css/**/*.css";
-paths.minCss = paths.webroot + "css/**/*.min.css";
-paths.concatJsDest = paths.webroot + "js/site.min.js";
-paths.concatCssDest = paths.webroot + "css/site.min.css";
+paths.generated = paths.webroot + "generated";
 
-gulp.task("clean:js", function (cb) {
-    rimraf(paths.concatJsDest, cb);
+gulp.task("clean:generated", function (cb) {
+	rimraf(paths.generated, cb);
 });
 
-gulp.task("clean:css", function (cb) {
-    rimraf(paths.concatCssDest, cb);
-});
+gulp.task("clean", ["clean:generated"]);
 
-gulp.task("clean", ["clean:js", "clean:css"]);
-
-gulp.task("min:js", function () {
-    return gulp.src([paths.js, "!" + paths.minJs], { base: "." })
-        .pipe(concat(paths.concatJsDest))
-        .pipe(uglify())
-        .pipe(gulp.dest("."));
-});
-
-gulp.task("min:css", function () {
-    return gulp.src([paths.css, "!" + paths.minCss])
-        .pipe(concat(paths.concatCssDest))
-        .pipe(cssmin())
-        .pipe(gulp.dest("."));
-});
-
-gulp.task("min", ["min:js", "min:css"]);
