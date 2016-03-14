baaa7be192803708a4551b97244512042d489525
JeroMiya <bell.jeremy@gmail.com>
2016-03-14 17:58:03 -0400

    Added angular-bootstrap and angular-animate. defined default route. Added bootstrap css reference.

--- src/ConfigUtil/wwwroot/app/modules.ts
+++ src/ConfigUtil/wwwroot/app/modules.ts
@@ -4,6 +4,10 @@
             "ui.router",
             "app.main",
             "app.landingPage"
-        ]);
+        ])
+        .config(["$stateProvider", "$urlRouterProvider",
+            ($stateProvider: ng.ui.IStateProvider, $urlRouterProvider: ng.ui.IUrlRouterProvider) => {
+                $urlRouterProvider.otherwise("/");
+            }]);
 
 }
