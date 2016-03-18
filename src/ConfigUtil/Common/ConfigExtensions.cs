c7027e59349c94fc04dce01cfa3fb96a69a6f4bc
JeroMiya <bell.jeremy@gmail.com>
2016-03-18 13:34:41 -0400

    Adding ConfigExtensions.cs to the repo.

---
+++ src/ConfigUtil/Common/ConfigExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace ConfigUtil.Common
+{
+    public static class ConfigExtensions
+    {
+        public static string GetOSVRServerDirectory(this IConfiguration config)
+        {
+            return config.Get<string>("OSVR_SERVER_ROOT", null);
+        }
+    }
+}
