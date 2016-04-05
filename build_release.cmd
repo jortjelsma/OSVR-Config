72aae31819bd0fd2e754bb350b5486caca401f99
JeroMiya <bell.jeremy@gmail.com>
2016-04-05 15:55:29 -0400

    Now using VS140COMNTOOLS env variable instead of a hard-coded path in build_release.cmd. Should fix the build.

--- build_release.cmd
+++ build_release.cmd
@@ -3,6 +3,6 @@ cd src\ConfigUtil
 @rem DNU is a batch file, so have to use call or we never return from this
 call dnu restore
 call dnu publish --runtime active --no-source -o ../../artifacts --configuration Release
-call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\devenv.exe" ..\Launcher\csproj /build Release
+call "%VS140COMNTOOLS%\..\IDE\devenv.exe" ..\Launcher\csproj /build Release
 xcopy ..\Launcher\bin\Release\OSVR-Config.exe ..\..\artifacts\ /y
 popd
