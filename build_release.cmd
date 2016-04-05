09bdc1eaf8b5ab8921298fc3bdf0394ca2d6aec9
JeroMiya <bell.jeremy@gmail.com>
2016-04-05 18:22:28 -0400

    Now calling vsvars32.bat and running msbuild. Fixes build on CI server.

--- build_release.cmd
+++ build_release.cmd
@@ -3,7 +3,8 @@ cd src\ConfigUtil
 @rem DNU is a batch file, so have to use call or we never return from this
 call dnu restore
 call dnu publish --runtime active --no-source -o ../../artifacts --configuration Release
-echo "Building Launcher using devenv: %VS140COMNTOOLS%\..\IDE\devenv.exe"
-call "%VS140COMNTOOLS%..\IDE\devenv.exe" ..\Launcher\Launcher.csproj /build Release
+call "%VS140COMNTOOLS%vsvars32.bat"
+call devenv ..\Launcher\Launcher.csproj /build Release
+msbuild ..\Launcher\Launcher.csproj /p:Configuration=Release
 xcopy ..\Launcher\bin\Release\OSVR-Config.exe ..\..\artifacts\ /y
 popd
